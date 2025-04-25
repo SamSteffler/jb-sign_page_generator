b796dfb50c76ed77da3687bd684350500232e42b
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-04-24 23:02:54 -0300

    criado leitor de csv inicial e protótipo de placa

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import sys
 import csv
+from PIL import Image, ImageDraw, ImageFont
 
 def main():
   if len(sys.argv) != 2:
     print("Uso: python script.py <arquivo.csv>")
-    sys.exit(1)
+    sys.exit(0)
 
   input_file = sys.argv[1]
 
@@ -16,6 +17,21 @@ def main():
     print(f"Erro: Arquivo '{input_file}' nao encontrado.")
   except Exception as e:
     print(f"Um erro ocorreu: {e}")
+
+  reader = csv.reader(file, delimiter=',')
+  for row in reader:
+    print(row)
+    code = row[0]
+    sciname = row[1]
+    commonname = row[2]
+
+  sign = Image.new('RGB', (2350, 1500), color = (0, 99, 0))
+  draw = ImageDraw.Draw(sign)
+  draw.rectangle([50, 50, 2300, 1450], fill=(0, 99, 0), outline=(255, 255, 255), width=50)
+  sign.show()
+  sign.save('sign.png')
+
+  file.close()
 
 if __name__ == "__main__":
   main()
