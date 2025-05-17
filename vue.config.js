7505b1c4b0c90770e029073892ea77214f1fbfc2
Samuel Steffler <samuel.steffler@acad.ufsm.br>
2025-05-17 11:12:07 -0300

    debugando

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/jb-sign_page_generator/" : "/vue_jb_static_pages/",
+  publicPath: process.env.NODE_ENV === "production" ? "/jb-sign_page_generator/item" : "/vue_jb_static_pages/",
 };
